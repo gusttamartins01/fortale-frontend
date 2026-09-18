383007f70a50bc6dc3421dbce3ec4b047d1849bc
gustavo martins <luizgustams@gmail.com>
2026-09-17 23:44:06 -0300

    refactor: modify and bring in project components.

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,15 @@
-import { useState } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Home from './pages/home/Home';
 
-function App() {
-	const [count, setCount] = useState(0);
-
+export default function App() {
 	return (
-		<>
-			<div className="w-full h-screen bg-gray-950 flex justify-center items-center gap-6 flex-col">
-				<h1 className="text-gray-200 font-bold text-6xl">Hello world!</h1>
+		<BrowserRouter>
+			<Navbar />
 
-				<button
-					type="button"
-					onClick={() => setCount(count + 1)}
-					className="bg-blue-600 px-8 py-4 rounded-2xl text-gray-200"
-				>
-					Number: <span className="text-gray-300">{count}</span>
-				</button>
-			</div>
-		</>
+			<main>
+				<Home />
+			</main>
+		</BrowserRouter>
 	);
 }
-
-export default App;
