94de9d7f5bebbaec8cac39c9e87a7749e48f3c79
gustavo martins <luizgustams@gmail.com>
2026-09-18 15:37:33 -0300

    refactor: Modify app routes for page display and semantic navigation.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Home from './pages/home/Home';
+import Profile from './pages/profile/Profile';
 
 export default function App() {
 	return (
@@ -8,7 +9,10 @@ export default function App() {
 			<Navbar />
 
 			<main>
-				<Home />
+				<Routes>
+					<Route path="/home" element={<Home />} />
+					<Route path="/profile" element={<Profile />} />
+				</Routes>
 			</main>
 		</BrowserRouter>
 	);
