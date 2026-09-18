6d321136b66ed0d43dfb7c716a1eab7259cdbe05
gustavo martins <luizgustams@gmail.com>
2026-09-17 23:43:02 -0300

    feat: create project navigation menu.

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,33 @@
+import { Link } from 'react-router-dom';
+import HomeImg from '../../assets/homeImg.png';
+
+export default function Home() {
+	return (
+		<section
+			className="relative min-h-screen bg-cover bg-center bg-no-repeat"
+			style={{ backgroundImage: `url(${HomeImg})` }}
+		>
+			<div className="pointer-events-none absolute inset-0 bg-black/40" />
+
+			<div className="relative z-10 flex min-h-screen flex-col items-center justify-center gap-5 text-center text-white">
+				<h1 className="text-6xl font-bold">FORTALÊ</h1>
+
+				<h2 className="text-3xl font-medium">
+					Tudo que Fortaleza tem pra oferecer, em um só lugar
+				</h2>
+
+				<p className="max-w-3xl text-lg">
+					Descubra praias, restaurantes, cafeterias, parques, cultura e eventos
+					- tudo em um só lugar, pra você explorar quando quiser.
+				</p>
+
+				<Link
+					to="/explore"
+					className="mt-5 bg-cyan-600 px-10 py-4 rounded-lg font-semibold transition hover:bg-cyan-700  duration-300 ease-in-out hover:scale-105"
+				>
+					Explorar Fortaleza
+				</Link>
+			</div>
+		</section>
+	);
+}
