5e1fd7d508dc2028e2bc59fc5d73427f126efba7
gustavo martins <luizgustams@gmail.com>
2026-09-20 00:20:06 -0300

    feat: creation of the home screen components.

---
+++ src/components/home/Testimonial.tsx
@@ -0,0 +1,11 @@
+export default function Testimonial() {
+	return (
+		<section className="h-auto w-full bg-gray-100 pt-10 pb-28 border-t-2 border-black/10">
+			<div className="mx-16 mt-16 flex items-center justify-start text-center">
+				<h2 className="rounded-t-2xl border-l-4 border-amber-600 pb-1 pl-5 pt-5 text-4xl font-bold text-amber-600">
+					O que as pessoas andam dizendo sobre nós
+				</h2>
+			</div>
+		</section>
+	);
+}
---
+++ src/pages/profile/Profile.tsx
@@ -0,0 +1,9 @@
+export default function Profile() {
+	return (
+		<section>
+			<div>
+				<h1>Olá!</h1>
+			</div>
+		</section>
+	);
+}
